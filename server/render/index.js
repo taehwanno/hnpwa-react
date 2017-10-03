c1194803d8ae8a141143ec33bf0ba0832a98cfba
Taehwan, No <taehwanno.dev@gmail.com>
2017-10-04 02:03:10 +0900

    Expose fetch polyfill to node.js global variable

--- server/render/index.js
+++ server/render/index.js
@@ -1,10 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const fetch = require('node-fetch');
 const render = require('./render').default; // eslint-disable-line
 
 const paths = require('../../paths');
 
 const template = fs.readFileSync(path.join(paths.build, 'index.html'), { encoding: 'utf-8' });
+
+global.fetch = fetch;
 
 module.exports = (ctx) => {
   const location = ctx.path;
