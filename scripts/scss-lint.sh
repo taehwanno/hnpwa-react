898694825affacf99172f205ae999bfec1885499
Taehwan, No <taehwanno.dev@gmail.com>
2017-10-18 01:51:05 +0900

    Improve scss lint script

--- scripts/scss-lint.sh
+++
@@ -1,5 +0,0 @@
-# ** THIS CANNOT BE EXECUTED IN NOT MAC (OS X) **
-# Mac OS X SourceTree cannot read system configuration related to $PATH, ~/.bash_profile, ~/.zshrc.
-# So, force to execute scss-lint (ruby modules) CLI with absolute path.
-
-$HOME/.rbenv/shims/scss-lint $@
