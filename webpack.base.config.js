a1c948feaf81cf2c17cf0a36bfe3a6bc5a74d245
Taehwan, No <taehwanno.dev@gmail.com>
2017-10-22 18:31:53 +0900

    Add runtime chunk in preload links

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -68,7 +68,7 @@ module.exports = {
     new PreloadWebpackPlugin({
       rel: 'preload',
       fileBlacklist: [/\.map/, /\.hot-update\.js$/],
-      include: ['app', 'vendor'],
+      include: ['app', 'runtime', 'vendor'],
     }),
     new PreloadWebpackPlugin(({
       rel: 'prefetch',
