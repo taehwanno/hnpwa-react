e8cdbe02d7fd4c17cbd7c6ab94b615863a728dc9
Taehwan, No <taehwanno.dev@gmail.com>
2018-01-21 19:33:07 +0900

    Show current page number in Pagination component

--- app/components/Pagination/index.jsx
+++ app/components/Pagination/index.jsx
@@ -16,9 +16,14 @@ const defaultProps = {
 
 function Pagination({ currentPage, onPaginate }) {
   const prevDisabled = currentPage === 1;
+  const nextDisabled = currentPage === 10;
 
   const prevButtonClassName = cx('Pagination__button', {
     'Pagination__button--disabled': prevDisabled,
+  });
+
+  const nextButtonClassName = cx('Pagination__button', {
+    'Pagination__button--disabled': nextDisabled,
   });
 
   return (
@@ -26,15 +31,19 @@ function Pagination({ currentPage, onPaginate }) {
       <div className="Pagination__inner">
         <button
           className={prevButtonClassName}
+          data-button="prev"
           disabled={prevDisabled}
           type="button"
           onClick={() => onPaginate(currentPage - 1)}
         >
           {'<'} Prev
         </button>
+        {currentPage} / 10
         {' '}
         <button
-          className="Pagination__button"
+          className={nextButtonClassName}
+          data-button="next"
+          disabled={nextDisabled}
           type="button"
           onClick={() => onPaginate(currentPage + 1)}
         >
