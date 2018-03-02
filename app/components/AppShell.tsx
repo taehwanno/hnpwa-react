2002a77484220fc8dbafd05151cea1b2faa7aec4
Taehwan, No <taehwanno.dev@gmail.com>
2018-03-02 22:49:38 +0900

    Migrate AppShell component to ts

--- app/components/AppShell.tsx
+++ app/components/AppShell.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import * as React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from 'components/Header';
-import { Feed, Item, User } from 'pages';
+import { Feed, Item, User } from '../pages';
 
 function AppShell() {
   return (
